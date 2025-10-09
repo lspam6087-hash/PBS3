d6d2a93b9800d0543d9666d4add77bf989120511
Lukas Neijts <l.neijts@student.tue.nl>
2025-10-09 10:29:20 +0200

    Inlcuded the calculations for the Chi parameter. It is not called in the main function yet

---
+++ parametercalc.c
@@ -0,0 +1,34 @@
+#include "parametercalc.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include "structs.h"
+
+// This function calculates the volume fraction of type A particles
+// Assumes that the volume of all of the beads (monomers) is the same
+volfrac_calc(struct Parameters *p_parameters, struct Vectors *p_vectors){
+    double phi_A = 0.0;
+    double *type = p_vectors->type;
+    double num_part = p_parameters->num_part;
+    double N_A = 0;
+
+    for (size_t i = 0; i < num_part; i++){
+        if (type[i] == 0){ // Assumes type A is represented by 0
+            N_A++;
+        }
+    }
+
+    phi_A = N_A / num_part;
+    return phi_A, N_A;
+}
+
+double chi_calculation(struct Parameters *p_parameters, struct Vectors *p_vectors){
+    double chi = 0.0;
+
+    double phi_A, N_A = volfrac_calc(p_parameters, p_vectors);
+
+    chi = (1/N_A) * (log((1-phi_A)/phi_A))/(1-2*phi_A);
+
+    return chi;
+}    
---
+++ parametercalc.h
@@ -0,0 +1,21 @@
+#ifndef PARAMETERCALC_H_
+#define PARAMETERCALC_H_
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+/**
+ * @brief Calculates the volume 
+ * 
+ * @return volume fraction of type A particles and number of type A particles
+ */
+double volfrac_calc(struct Parameters *p_parameters, struct Vectors *p_vectors);
+
+/**
+ * @brief Calculates the Chi parameter wih the volume fraction of type A particles 
+ * 
+ * @return Chi parameter
+ */
+double chi_calculation(struct Parameters *p_parameters, struct Vectors *p_vectors);
+
+#endif 
