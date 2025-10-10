d422eb4f37bc10f4b38b37a1e41672a0d15d6f6c
Lukas Neijts <l.neijts@student.tue.nl>
2025-10-10 16:34:57 +0200

    Deleted some comments to clean up the file

--- plotting_dens.py
+++ plotting_dens.py
@@ -1,28 +1,3 @@
-# import pandas as pd
-# import matplotlib.pyplot as plt
-
-# df = pd.read_csv("data\dens_histogramC2.csv")
-
-# # Assign generic column names
-
-# print(df.head())
-# # Plot the three series against col_0
-# plt.figure(figsize=(8,6))
-# plt.plot(df["x_center"], df["density_A"], label='Rho_A', marker='o')
-# # plt.plot(data['col_0'], data['col_2'], label='Series 2', marker='s')
-# # plt.plot(data['col_0'], data['col_3'], label='Series 3', marker='^')
-
-# plt.xlabel("col_0")
-# plt.ylabel("Values")
-# plt.title("Line Plot of Data")
-# plt.legend()
-# plt.grid()
-# plt.show()
-
-# It seems there might be hidden characters or whitespace in the column names.
-# Let's inspect the column names to confirm and clean them up before plotting.
-
-# Reload file
 import pandas as pd
 import matplotlib.pyplot as plt
 file_path = "data/dens_histogramC2.csv"
